02171a9b63c4db09ff33b36a0d9e7dd187f774ad
Ola-Domain <olasupooladotun6@gmail.com>
2022-09-08 12:53:35 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- *main - print the string in the put function
- *
- *Description: using prints "Programming is like builing a multilingual puzzel
- *Return: 0
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-}
-	printf("with proper grammer, but the outcome is a piece of art,\");
-	return (0);
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
