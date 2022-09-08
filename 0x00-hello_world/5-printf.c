9ddd2108232a4f951fed25bed1fe7f99aaae0543
Ola-Domain <olasupooladotun6@gmail.com>
2022-09-08 10:52:50 -0700

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "Programming is like builing a multilingual puzzel
-*Return: 0
-*/
+ *main - print the string in the put function
+ *
+ *Description: using prints "Programming is like builing a multilingual puzzel
+ *Return: 0
 int main(void)
-{
-	puts("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+}
+	printf("with proper grammer, but the outcome is a piece of art,\");
+	return (0);
 }
