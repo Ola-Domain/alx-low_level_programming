5f6ac04a17dfadcb0346e40f82cdfba58b1564a1
Ola-Domain <olasupooladotun6@gmail.com>
2022-10-10 08:29:45 -0700

    File-name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-
 /**
  * main - print the name of the file that was compiled
  * Return: int
  */
- 
+
 int main(void)
 {
-    printf("Name of source file: %s\n", __FILE__);
-
-    return 0;
+printf("%s\n", __FILE__);
+return (0);
 }
